3b30527651ad58da059e77ce89ed31a58fe27f47
Sanjeev Kumar <sanjeevkumar@me.com>
2022-02-20 01:38:53 +1100

    Code to Capture manager and team meber details and creating respective objects.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
-let teamDetailsInput = [];
-let teamMembers = [];
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+let teamManagerDetailsInput = [];
+let teamMembersInput = [];
+let teamManager;
 
 // TODO: Create a function to initialize app and start capture of user input for team details.
 function init() {
@@ -14,40 +17,104 @@ function init() {
 // Capture team manager's details
 
 function getTeamDetails() {
+    getAndSetManagerDetails();
+}
+
+function getAndSetManagerDetails() {
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: `Please enter team manager's name.`,
+                message: `As part of team creation, enter team manager's name.`,
             },
             {
                 type: 'input',
                 name: 'employeeID',
-                message: `Please enter team manager's emplopyee ID..`,
+                message: `Enter team manager's emplopyee ID..`,
             },
             {
                 type: 'input',
                 name: 'emailAddress',
-                message: `Please team manager's email address.`,
+                message: `Enter team manager's email address.`,
             },
             {
                 type: 'input',
                 name: 'officeNumber',
-                message: `Please enter team manager's office number.`,
+                message: `Enter team manager's office number.`,
             },
         ])
         .then((data) => {
-            teamDetailsInput.push(data);
-            createTeamProfile();
-            prepareIndexHtml();
+            teamManagerDetailsInput.push(data);
+            // createTeamProfile();
+            // prepareIndexHtml();
+            addTeamMembers();
         });
 
 }
 
+// let insertEngineerYesInternNo = true;
+const teamMemberType = ['Engineer', 'Intern']
+const moreTeamMembers = ['Yes', 'No'];
+
+function addTeamMembers() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Choose Engineer or Intern to be added as part of your team.',
+                name: 'teamMemberType',
+                choices: teamMemberType,
+            },
+            {
+                type: 'input',
+                name: 'name',
+                message: `Enter name of your team member.`,
+            },
+            {
+                type: 'input',
+                name: 'employeeID',
+                message: `Enter  employee ID of team member.`,
+            },
+            {
+                type: 'input',
+                name: 'emailAddress',
+                message: `Enter employee email address.`,
+            },
+            {
+                type: 'input',
+                name: 'schoolNameOrGithubUsername',
+                message: `Enter school name for intern or github username for engineer.`,
+            },
+            {
+                type: 'list',
+                message: 'Choose Yes or No, if more team members needs to be added.',
+                name: 'nextTeamMember',
+                choices: moreTeamMembers,
+            },
+        ])
+        .then((data) => {
+            teamMembersInput.push(data);
+            if (data.nextTeamMember === 'Yes') {
+                addTeamMembers();
+            }
+            else {
+                createTeamProfile();
+                prepareIndexHtml();
+            }
+        });
+}
+
 function createTeamProfile() {
-    const teamManager = new Manager(teamDetailsInput[0].name, teamDetailsInput[0].employeeID, teamDetailsInput[0].emailAddress, teamDetailsInput[0].officeNumber );
-    teamManager.addTeamMember('first');
+    teamManager = new Manager(teamManagerDetailsInput[0].name, teamManagerDetailsInput[0].employeeID, teamManagerDetailsInput[0].emailAddress, teamManagerDetailsInput[0].officeNumber);
+    teamMembersInput.forEach(element => {
+        if (element.teamMemberType == 'Engineer') {
+            teamManager.addTeamMember(new Engineer(element.name, element.employeeID, element.emailAddress, element.schoolNameOrGithubUsername));
+        }
+        else {
+            teamManager.addTeamMember(new Intern(element.name, element.employeeID, element.emailAddress, element.schoolNameOrGithubUsername));
+        }
+    });
     console.log(teamManager);
 }
 
@@ -55,7 +122,7 @@ function createTeamProfile() {
 
 function prepareIndexHtml() {
     const fileName = './dist/index.html';
-    writeToFile(fileName, JSON.stringify(teamDetailsInput));
+    writeToFile(fileName, JSON.stringify(teamManager));
 }
 
 function writeToFile(fileName, data) {
